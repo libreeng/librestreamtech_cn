26f695ee7c677c955055d9debbb7423624d4c0fa
John Graumann <john.graumann@librestream.com>
2023-05-12 09:25:28 -0500

    Added support for redirecting to branch deep links

--- scripts/librestreamtech-redirect.js
+++ scripts/librestreamtech-redirect.js
@@ -2,10 +2,15 @@ function parseRedirectParams(redirectLink) {
     const urlParams = new URLSearchParams(window.location.search);
     const param = urlParams.get('p');
     const environment = urlParams.get('e');
+    const deeplink = (urlParams.get('d') === 'true');
 
     var url = "https://onsight.librestream.com/";
-    if (environment == "vld") {
-        url = "https://oam.vld.libreeng.com/"
+    if (deeplink) {
+        url = "https://link.onsight.librestream.com/"
+    } else {
+        if (environment == "vld") {
+            url = "https://oam.vld.libreeng.com/"
+        }
     }
 
     if (param !== null) {
